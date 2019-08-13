2abd301ca193444f494e3f7f73ba911b417bac74
Denizoguzatalay <denizoguz.us@gmail.com>
2019-08-13 16:36:56 -0500

    firstbranch first

--- src/test/java/Practice/Practice2.java
+++ src/test/java/Practice/Practice2.java
@@ -4,6 +4,8 @@ public class Practice2 {
     public static void main(String[] args) {
         System.out.println("ADAMSIN");
         System.out.println("Zalimsin github");
+        System.out.println("firstbranch change 1");
+
     }
 
 }
