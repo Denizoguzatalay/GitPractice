c49580b8ea305e80efa8aace47b5561563ede497
Denizoguzatalay <denizoguz.us@gmail.com>
2019-08-13 15:34:31 -0500

    adamsin coflict same line

--- src/test/java/Practice/Practice2.java
+++ src/test/java/Practice/Practice2.java
@@ -2,7 +2,7 @@ package Practice;
 
 public class Practice2 {
     public static void main(String[] args) {
-        System.out.println("ddd");
+        System.out.println("Zalimsin github");
     }
 
 }
