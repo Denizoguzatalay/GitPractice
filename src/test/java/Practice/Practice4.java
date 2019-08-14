47db7a06e9505e397466a4d84cf3d4e89ff34f4d
Denizoguzatalay <51774134+Denizoguzatalay@users.noreply.github.com>
2019-08-13 19:15:43 -0500

    Practice 4 updates

--- src/test/java/Practice/Practice4.java
+++ src/test/java/Practice/Practice4.java
@@ -1,1 +1,12 @@
 
+package Practice;
+
+public class Practice3 {
+
+    public static void main(String[] args) {
+        System.out.println("12456");
+        System.out.println("12456");
+    }
+
+
+}
