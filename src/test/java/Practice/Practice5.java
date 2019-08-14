6150f18461c10b2a8ebdaf89d0c18adfbc62a83a
Denizoguzatalay <denizoguz.us@gmail.com>
2019-08-13 19:37:02 -0500

    firstbranch latest change1

---
+++ src/test/java/Practice/Practice5.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Practice5 {
+    public static void main(String[] args) {
+        System.out.println("jagsaxsxhgasvxhgagvxc");
+        System.out.println("jagsaxsxhgasvxhgagvxc");
+
+        System.out.println("jagsaxsxhgasvxhgagvxc");
+        System.out.println("jagsaxsxhgasvxhgagvxc");
+        System.out.println("jagsaxsxhgasvxhgagvxc");
+        System.out.println("jagsaxsxhgasvxhgagvxc");
+    }
+}
