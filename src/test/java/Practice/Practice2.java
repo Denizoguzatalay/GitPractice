c6dfce4738989bf12bd25def8af0bb9e82e410ac
Denizoguzatalay <51774134+Denizoguzatalay@users.noreply.github.com>
2019-08-14 16:12:33 -0500

    practice 2 change

--- src/test/java/Practice/Practice2.java
+++ src/test/java/Practice/Practice2.java
@@ -2,17 +2,8 @@ package Practice;
 
 public class Practice2 {
     public static void main(String[] args) {
-        System.out.println("ADAMSIN");
-        System.out.println("Zalimsin github");
-        System.out.println("firstbranch change 1");
-         System.out.println("firstbranch change 4");
-         System.out.println("firstbranch change 5");
-         System.out.println("firstbranch change 6");
-
-
-        System.out.println("firstbranch change 2");
-        System.out.println("firstbranch change 3");
-
+        System.out.println("bbbbbbb");
+        
 
     }
 
