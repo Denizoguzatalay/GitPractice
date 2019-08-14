a67b510acfc2d324a174e27f46a3fa15b015a61b
Denizoguzatalay <denizoguz.us@gmail.com>
2019-08-13 19:53:40 -0500

    last change

--- src/test/java/Practice/Practice3.java
+++ src/test/java/Practice/Practice3.java
@@ -5,6 +5,14 @@ public class Practice3 {
     public static void main(String[] args) {
         System.out.println("8765");
         System.out.println("svxhxhasv");
+
+
+        System.out.println("8765");
+        System.out.println("svxhxhasv");
+        System.out.println("8765");
+        System.out.println("svxhxhasv");
+        System.out.println("8765");
+        System.out.println("svxhxhasv");
     }
 
 
