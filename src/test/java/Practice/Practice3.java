67648bd831624e941862f626642b689552ae2e41
Denizoguzatalay <denizoguz.us@gmail.com>
2019-08-13 19:18:19 -0500

    secondbranch first change

---
+++ src/test/java/Practice/Practice3.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class Practice3 {
+
+    public static void main(String[] args) {
+        System.out.println("8765");
+        System.out.println("svxhxhasv");
+    }
+
+
+}
