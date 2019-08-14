c75ff9eaf196b86efc448e5523414d0fdb8e7036
Denizoguzatalay <51774134+Denizoguzatalay@users.noreply.github.com>
2019-08-13 19:33:41 -0500

    second branch second change

--- src/test/java/Practice/Practice4.java
+++ src/test/java/Practice/Practice4.java
@@ -6,6 +6,10 @@ public class Practice3 {
     public static void main(String[] args) {
         System.out.println("12456");
         System.out.println("12456");
+        System.out.println("12456");
+        System.out.println("12456");
+        System.out.println("12456");
+        System.out.println("12456");
     }
 
 
