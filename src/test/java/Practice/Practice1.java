54ef41ec1e5b4ae9e9f9ebaa9cfcc801f2e9c439
Denizoguzatalay <denizoguz.us@gmail.com>
2019-08-14 22:23:33 -0500

    second

--- src/test/java/Practice/Practice1.java
+++ src/test/java/Practice/Practice1.java
@@ -3,22 +3,13 @@ package Practice;
 public class Practice1 {
 
     public static void main(String[] args) {
-        System.out.println("aaaaaa");
+
         
         System.out.println("aaaa");
         System.out.println("dddd");
         System.out.println("dddd");
 
-        System.out.println("dddd");
-        System.out.println("lllllllllllllll");
-        System.out.println("dddd");
-        System.out.println("dddd");
-        System.out.println("mmmmmmmmmmmmmmmm");
-        System.out.println("dddd");
-        System.out.println("lllllllllllllll");
-        System.out.println("dddd");
-        System.out.println("dddd");
-        System.out.println("mmmmmmmmmmmmmmmm");
+
 
 
 
